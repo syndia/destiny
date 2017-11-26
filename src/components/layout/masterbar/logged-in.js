920cfbef11eae564b4c1f78c9232191909a1a59b
Syndia <johnripke@syndia.nl>
2017-11-26 17:51:16 +0100

    [UPDATE] Logged In Masterbar

--- src/components/layout/masterbar/logged-in.js
+++ src/components/layout/masterbar/logged-in.js
@@ -6,24 +6,46 @@ import React, { Component } from 'react'
 /**
  * Internal dependencies
  */
+import { rhythm, scale } from '../../../utils/typography'
 import { getMembershipsForCurrentUser } from '../../../services/bungie-net/api/user'
 import Masterbar from './index'
 import Item from './item'
 
 export default class MasterbarLoggedIn extends Component {
   componentDidMount() {
-    this.fetchCharacters()
+    this.fetchMemberships()
   }
 
-  fetchCharacters = () => {
-    getMembershipsForCurrentUser().then(data => console.log(data))
+  fetchMemberships = () => {
+    getMembershipsForCurrentUser()
+      .then(data => {
+        if (!data) {
+          return
+        }
+
+        this.setState({ data })
+      })
+      .catch(error => console.error(error))
   }
 
   render() {
     const { title } = this.props
     return (
       <Masterbar>
-        <Item className="masterbar__item-title">{title}</Item>
+        <Item className="masterbar__item-title">
+          {' '}
+          <h1
+            css={{
+              display: `inline-block`,
+              margin: 0,
+              verticalAlign: `middle`,
+              ...scale(2 / 5),
+            }}
+          >
+            {title || `Syndia`}
+          </h1>
+        </Item>
+        <Item />
       </Masterbar>
     )
   }
