c60883bb028c6884abc65835e414b5bdc681837f
Syndia <johnripke@syndia.nl>
2017-12-03 17:46:33 +0100

    [FIX] Import constant

--- src/state/memberships/actions.js
+++ src/state/memberships/actions.js
@@ -5,6 +5,7 @@ import {
   MEMBERSHIPS_FOR_CURRENT_USER_FETCH,
   MEMBERSHIPS_FOR_CURRENT_USER_FETCH_FAILED,
   MEMBERSHIPS_FOR_CURRENT_USER_FETCH_SUCCESS,
+  MEMBERSHIPS_FOR_CURRENT_USER_RECEIVE,
 } from '../action-types'
 import { setCurrentUserId } from '../current-user/actions'
 import { getMembershipsForCurrentUser } from '../../services/bungie-net/api/user'
